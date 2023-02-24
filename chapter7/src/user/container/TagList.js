cdf8d3122f1dc9384096997cb9d5f2a5b36ce7f2
dev_sm <tmdals9974@naver.com>
2023-02-24 16:15:16 +0900

    feat: 7-8강 학습내용 정리

--- chapter7/src/user/container/TagList.js
+++ chapter7/src/user/container/TagList.js
@@ -1,5 +1,62 @@
-import React from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import { Input, message, Tag } from "antd";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actions } from "../state";
 
 export default function TagList() {
-  return <>태그</>;
+  const user = useSelector((state) => state.user.user);
+  const tags = user?.tag ? user.tag.split(",").map((item) => item.trim()) : [];
+  const dispatch = useDispatch();
+
+  const [isAdd, setIsAdd] = useState(false);
+  const [tempTag, setTempTag] = useState("");
+  function onAdd() {
+    setIsAdd(true);
+    setTempTag("");
+  }
+
+  function onSave() {
+    if (!tempTag) {
+      setIsAdd(false);
+    } else if (tags.includes(tempTag)) {
+      message.error("이미 같은 태그가 있습니다.");
+    } else {
+      const newTag = user?.tag ? `${user.tag}, ${tempTag}` : tempTag;
+      dispatch(actions.fetchUpdateUser({ user, key: "tag", value: newTag, fetchKey: "tag" }));
+      setIsAdd(false);
+    }
+  }
+
+  function onDelete(tag) {
+    const newTag = tags.filter((item) => item !== tag).join(", ");
+    dispatch(actions.fetchUpdateUser({ user, key: "tag", value: newTag, fetchKey: "tag" }));
+  }
+
+  return (
+    <>
+      {tags.map((item) => (
+        <Tag key={item} closable onClose={() => onDelete(item)}>
+          {item}
+        </Tag>
+      ))}
+      {!isAdd && (
+        <Tag onClick={onAdd}>
+          <PlusOutlined /> New Tag
+        </Tag>
+      )}
+      {isAdd && (
+        <Input
+          autoFocus
+          type="text"
+          size="small"
+          style={{ width: 100 }}
+          value={tempTag}
+          onChange={(e) => setTempTag(e.target.value)}
+          onBlur={() => setIsAdd(false)}
+          onPressEnter={onSave}
+        />
+      )}
+    </>
+  );
 }
--- chapter7/src/user/container/User.js
+++ chapter7/src/user/container/User.js
@@ -7,6 +7,7 @@ import useFetchInfo from "../../common/hook/useFetchInfo";
 import History from "../../common/component/History";
 import Department from "./Department";
 import TagList from "./TagList";
+import FetchLabel from "../component/FetchLabel";
 /**
  *
  * @param {object} param
@@ -22,28 +23,26 @@ export default function User({ match }) {
     dispatch(actions.fetchUser(name));
   }, [dispatch, name]);
 
-  const { isFetched, isSlow } = useFetchInfo(Types.FetchUser);
+  const { isFetched } = useFetchInfo(Types.FetchUser);
 
   return (
     <Row justify="center">
       <Col xs={24} md={20} lg={14}>
-        <PageHeader
-          onBack={history.goBack}
-          title={
-            <Space>
-              사용자 정보
-              {isSlow && <Spin size="small" />}
-            </Space>
-          }
-        >
+        <PageHeader onBack={history.goBack} title={<FetchLabel label="사용자 정보" actionType={Types.FetchUser} />}>
           {user && (
             <Descriptions layout="vertical" bordered column={1}>
               <Descriptions.Item label="이름">
                 <Typography.Text>{user.name}</Typography.Text>
               </Descriptions.Item>
-              <Descriptions.Item label="소속"><Department /></Descriptions.Item>
-              <Descriptions.Item label="태그"><TagList /></Descriptions.Item>
-              <Descriptions.Item label="수정 내역"><History /></Descriptions.Item>
+              <Descriptions.Item label={<FetchLabel label="소속" actionType={Types.FetchUpdateUser} fetchKey="department" />}>
+                <Department />
+              </Descriptions.Item>
+              <Descriptions.Item label={<FetchLabel label="태그" actionType={Types.FetchUpdateUser} fetchKey="tag" />}>
+                <TagList />
+              </Descriptions.Item>
+              <Descriptions.Item label="수정 내역">
+                <History />
+              </Descriptions.Item>
             </Descriptions>
           )}
           {!user && isFetched && <Typography.Text>존재하지 않는 사용자입니다.</Typography.Text>}
